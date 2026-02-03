983d1b3f5e3152a5b3e61e7bf7d77fa4a0e99537
NKMODNK7 <botbabbu68@gmail.com>
2026-02-03 23:31:30 +0530

    Update autoleave.py

--- AviaxMusic/plugins/misc/autoleave.py
+++ AviaxMusic/plugins/misc/autoleave.py
@@ -1,7 +1,7 @@
 import asyncio
 from datetime import datetime
 from pyrogram.enums import ChatType
-from pytgcalls.exceptions import NoActiveGroupCall
+from pytgcalls.exceptions import GroupCallNotFound
 import config
 from AviaxMusic import app
 from AviaxMusic.misc import db
@@ -10,7 +10,7 @@ from AviaxMusic.utils.database import get_client, set_loop, is_active_chat, is_a
 import logging
 
 async def auto_leave():
-    while not await asyncio.sleep(8400):
+    while not await asyncio.sleep(900):
         from AviaxMusic.core.userbot import assistants
         ender = await is_autoleave()
         if not ender:
@@ -51,57 +51,37 @@ async def auto_end():
             ender = await is_autoend()
             if not ender:
                 continue
-
             chatss = autoend
             keys_to_remove = []
-
+            nocall = False
             for chat_id in chatss:
-                nocall = False
                 try:
-                    assistant = await group_assistant(Aviax, chat_id)
-                    participants = await assistant.get_participants(chat_id)
-                    users = len(participants)
-                except NoActiveGroupCall:
+                    users = len(await Aviax.call_listeners(chat_id))
+                except GroupCallNotFound:
                     users = 1
                     nocall = True
                 except Exception:
                     users = 100
-
                 timer = autoend.get(chat_id)
-                if users == 1 and isinstance(timer, datetime):
-                    if datetime.now() >= timer:
-                        await set_loop(chat_id, 0)
-                        keys_to_remove.append(chat_id)
-
-                        try:
-                            await db[chat_id][0]["mystic"].delete()
-                        except Exception:
-                            pass
-
-                        try:
-                            await Aviax.stop_stream(chat_id)
-                        except Exception:
-                            pass
-
-                        try:
-                            if not nocall:
-                                await app.send_message(
-                                    chat_id,
-                                    "» ʙᴏᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇғᴛ ᴠɪᴅᴇᴏᴄʜᴀᴛ "
-                                    "ʙᴇᴄᴀᴜsᴇ ɴᴏ ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-                                )
-                        except Exception:
-                            pass
-
+                if users == 1:
+                    res = await set_loop(chat_id, 0)
+                    keys_to_remove.append(chat_id)
+                    try:
+                        await db[chat_id][0]["mystic"].delete()
+                    except Exception:
+                        pass
+                    try:
+                        await Aviax.stop_stream(chat_id)
+                    except Exception:
+                        pass
+                    try:
+                        if not nocall:
+                            await app.send_message(chat_id, "» ʙᴏᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇғᴛ ᴠɪᴅᴇᴏᴄʜᴀᴛ ʙᴇᴄᴀᴜsᴇ ɴᴏ ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.")
+                    except Exception:
+                        pass
             for chat_id in keys_to_remove:
-                try:
-                    del autoend[chat_id]
-                except Exception:
-                    pass
-
+                del autoend[chat_id]
         except Exception as e:
             logging.info(e)
 
-
 asyncio.create_task(auto_end())
-
